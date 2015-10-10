5a6ccd0ba253e1604e407f62a3047b795c83c604
Chris Lewis <Bonsitm@gmail.com>
2015-10-09 18:02:41 -0700

    Add first test

--- test-app/src/main.c
+++ test-app/src/main.c
@@ -5,25 +5,24 @@
 static Window *s_window;
 static Layer *s_layer;
 
-static void invert(GContext *ctx, GColor c1, GColor c2) {
-  GBitmap *fb = graphics_capture_frame_buffer(ctx);
+static void test(bool condition, char *name) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Test \"%s\" %s", 
+          name, condition ? "PASSED" : "FAILED");
+}
 
-  universal_fb_swap_colors(fb, c1, c2);
-
-  universal_fb_set_pixel_color(fb, GPoint(70, 70), PBL_IF_COLOR_ELSE(GColorBlue, GColorBlack));
-
-  // Release framebuffer
-  graphics_release_frame_buffer(ctx, fb);
+static void test_drawing(GBitmap *fb, GColor c1, GColor c2) {
+  // Test SET
+  universal_fb_set_pixel_color(fb, GPoint(30, 30), c1);
+  test(gcolor_equal(universal_fb_get_pixel_color(fb, GPoint(30, 30)), c1), "universal_fb_set/get_pixel_color");
 }
 
 static void update_proc(Layer *layer, GContext *ctx) {
   GColor c1 = PBL_IF_COLOR_ELSE(GColorRed, GColorBlack);
   GColor c2 = PBL_IF_COLOR_ELSE(GColorGreen, GColorWhite);
 
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorBlack));
-  graphics_fill_rect(ctx, GRect(30, 30, 30, 30), GCornerNone, 0);
-
-  invert(ctx, c1, c2);
+  GBitmap *fb = graphics_capture_frame_buffer(ctx);
+  test_drawing(fb, c1, c2);
+  graphics_release_frame_buffer(ctx, fb);
 }
 
 static void window_load(Window *window) {
